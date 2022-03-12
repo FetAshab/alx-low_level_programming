54ea67242493ee61d22767eab1a617e7b8f41dd3
FetAshab <fethulm@gmail.com>
2022-03-11 17:57:35 -0800

    print possible combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,15 +8,15 @@ int main(void)
 {
 		int i;
 
-			for (i = 0; i < 10; i++)
+		for (i = 0; i < 10; i++)
+		{
+			putchar(i % 10 + '0');
+			if (i < 9)
 			{
-				putchar(i % 10 + '0');
-				if (i < 9)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-		    	}
-			putchar('\n');
-			return (0);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+		return (0);
 }
