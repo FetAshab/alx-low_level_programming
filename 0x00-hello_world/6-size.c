c1d5a05e230b4c7463a570cb5261ee70e617b2ae
FetAshab <fethulm@gmail.com>
2022-03-14 05:31:05 -0700

    print sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,5 +18,6 @@ int main(void)
 	 printf("Size of a long int: %lu byte(s)\n", sizeof(c_long));
 	 printf("Size of a long long int: %lu byte(s)\n", sizeof(c_long_long));
 	 printf("Size of a float: %lu byte(s)\n", sizeof(c_float));
+
 	 return (0);
 }
