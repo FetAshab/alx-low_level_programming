da9330656eec4f4b81223e2e40d91023423c02ca
FetAshab <fethulm@gmail.com>
2022-03-15 13:30:38 -0700

    check whether it is lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,15 +3,15 @@
  *  * _islower - returns 1 if lowercase alphabet and 0 otherwise
  *   * @c: takes in a character
  *    * Return: 0 for uppercase, 1 for lowercase
- *     */
+ */
 int _islower(int c)
 {
 		if (c >= 'a' && c <= 'z')
-				{
-							return (1);
-								}
-			else
-					{
-								return (0);
-									}
+			{
+				return (1);
+			}
+		else
+			{
+				return (0);
+			}
 }
