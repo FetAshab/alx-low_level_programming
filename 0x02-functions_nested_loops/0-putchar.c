fb4c5e6d4ca1252c2f03fd620c6d8f1fd8e26975
FetAshab <fethulm@gmail.com>
2022-03-15 12:57:42 -0700

    prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,16 +7,14 @@
  */
 int main(void)
 {
-	_putchar('_');
- 	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char put[] = "_putchar";
 
+	int a;
+
+	for (a = 0; a < 8; a++)
+	{					
+		_putchar(put[a]);
+	}
+	_putchar('\n');
 	return (0);
 }
